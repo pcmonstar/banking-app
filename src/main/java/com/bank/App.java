7c7bcda08764b01b4ef8d52f9fb1241b72280e3f
Prashant Choudhary <pcmonstar@gmail.com>
2026-07-07 18:50:14 +0530

    Initial banking application

---
+++ src/main/java/com/bank/App.java
@@ -0,0 +1,23 @@
+package com.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("==================================");
+        System.out.println("Banking Application");
+        System.out.println("Version : v1");
+        System.out.println("Environment : DEV");
+        System.out.println("Application Started Successfully");
+        System.out.println("==================================");
+
+        while (true) {
+            Thread.sleep(10000);
+            System.out.println("Application is running...");
+        }
+    }
+}
