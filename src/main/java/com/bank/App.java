f56cace9b02db592cd6c190760d6b67d8fbc3abc
Prashant Choudhary <pcmonstar@gmail.com>
2026-07-14 10:10:06 +0530

    testing git fetch

--- src/main/java/com/bank/App.java
+++ src/main/java/com/bank/App.java
@@ -10,7 +10,7 @@ public class App {
 
         System.out.println("==================================");
         System.out.println("Banking Application");
-        System.out.println("Version : Feature Branch");
+        System.out.println("Version : Fetch Test");
 	    System.out.println("Build Number : 2");
         System.out.println("Environment : DEVELOPMENT");
         System.out.println("Application Started Successfully. Build from Jenkins Pipeline");
